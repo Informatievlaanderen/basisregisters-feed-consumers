1b29582a6739477594f4102c977a91f53b913968
Arne Dumarey <dumarey.arne@gmail.com>
2026-04-20 10:19:32 +0200

    wip: feat: initial postalinfo structure

---
+++ src/Basisregisters.FeedConsumers.Console/PostalInformation/PostalInformationAttributes.cs
@@ -0,0 +1,8 @@
+﻿namespace Basisregisters.FeedConsumers.Console.PostalInformation;
+
+public sealed class PostalInformationAttributes
+{
+    public const string Status = "postInfoStatus";
+    public const string MunicipalityId = "gemeente.id";
+    public const string Names = "postnamen";
+}
---
+++ src/Basisregisters.FeedConsumers.Console/PostalInformation/PostalInformationProjector.cs
@@ -0,0 +1,17 @@
+﻿namespace Basisregisters.FeedConsumers.Console.PostalInformation;
+
+using System;
+using Model;
+
+public sealed class PostalInformationProjector
+{
+    private static PostalInformationStatus MapStatus(string status)
+    {
+        return status switch
+        {
+            "gerealiseerd" => PostalInformationStatus.Realized,
+            "gehistoreerd" => PostalInformationStatus.Retired,
+            _ => throw new ArgumentException($"Unknown status: {status}")
+        };
+    }
+}

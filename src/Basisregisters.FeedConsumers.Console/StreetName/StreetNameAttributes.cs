8022d583e344cfea3eb476f948da8fcddc9562d0
Arne Dumarey <dumarey.arne@gmail.com>
2026-07-28 11:48:43 +0200

    refactor(streetname): change oslo

--- src/Basisregisters.FeedConsumers.Console/StreetName/StreetNameAttributes.cs
+++ src/Basisregisters.FeedConsumers.Console/StreetName/StreetNameAttributes.cs
@@ -2,8 +2,8 @@ namespace Basisregisters.FeedConsumers.Console.StreetName;
 
 public static class StreetNameAttributes
 {
-    public const string Status = "straatnaamStatus";
-    public const string Names = "straatnamen";
-    public const string HomonymAdditions = "homoniemToevoegingen";
-    public const string MunicipalityId = "gemeente.id";
+    public const string Status = "status";
+    public const string Names = "straatnaam";
+    public const string HomonymAdditions = "homoniemToevoeging";
+    public const string AssignedBy = "isToegekendDoor";
 }
